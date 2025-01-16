2bb8623e91b2ffa865c4ace10b8ab34c5ad026b0
Giedo Stolk <giedo.stolk@ns.nl>
2025-01-16 08:44:44 +0100

    In aparte data dirs

--- bitvavo-scalper/run.sh
+++ bitvavo-scalper/run.sh
@@ -14,12 +14,16 @@ fi
 
 echo "Running Docker container with configuration: $CONFIG"
 
+mkdir -p $(pwd)/data/${CONFIG}
+touch $(pwd)/data/${CONFIG}/bot_status.json
+touch $(pwd)/data/${CONFIG}/transactions.json
+
 docker run --name scalper_${CONFIG} -d \
   -v $(pwd)/config/config.json:/app/config.json \
   -v $(pwd)/config/${CONFIG}.json:/app/scalper.json \
   -v $(pwd)/config/slack.json:/app/slack.json \
-  -v $(pwd)/data/bot_status.json:/app/bot_status.json \
-  -v $(pwd)/data/transaction.json:/app/transactions.json \
+  -v $(pwd)/data/${CONFIG}/bot_status.json:/app/bot_status.json \
+  -v $(pwd)/data/${CONFIG}/transactions.json:/app/transactions.json \
   bitvavo-scalper:latest
 sleep 30
 docker logs scalper_${CONFIG}
